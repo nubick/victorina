7e5676e49c6eaef3fe3372b2802c8460cdad27fc
Oleg Andrushko <nubick@gmail.com>
2020-12-25 17:23:27 +0800

    Decode/encode ip to code and back, test framework.

---
+++ UnityProject/Assets/Scripts/Network/IpCodeSystem.cs
@@ -0,0 +1,79 @@
+using System.Net;
+using System.Text;
+using UnityEngine;
+
+namespace Victorina
+{
+    public class IpCodeSystem
+    {
+        private const int LettersAmount = 26;
+        
+        public string GetCode(string ipString)
+        {
+            byte[] ipBytes = GetIpBytes(ipString);
+            string code = GetCode(ipBytes);
+            Debug.Log($"{ipString} {code}");
+            return code;
+        }
+        
+        private byte[] GetIpBytes(string ipString)
+        {
+            if (IPAddress.TryParse(ipString, out var ipAddress))
+            {
+                return ipAddress.GetAddressBytes();
+            }
+            return null;
+        }
+        
+        private string GetCode(byte[] ipNumbers)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte number in ipNumbers)
+            {
+                int letterNumber = number % LettersAmount;
+                int letterCircles = number / LettersAmount;
+                sb.Append((char) ('A' + letterNumber));
+                sb.Append((char) ('A' + letterCircles));
+            }
+            return sb.ToString();
+        }
+
+        public string GetIp(string code)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(GetNumber(code[0], code[1]));
+            sb.Append('.');
+            sb.Append(GetNumber(code[2], code[3]));
+            sb.Append('.');
+            sb.Append(GetNumber(code[4], code[5]));
+            sb.Append('.');
+            sb.Append(GetNumber(code[6], code[7]));
+            return sb.ToString();
+        }
+
+        private int GetNumber(char ch1, char ch2)
+        {
+            int n1 = ch1 - 'A';
+            int n2 = ch2 - 'A';
+            return n2 * LettersAmount + n1;
+        }
+
+        public bool IsValidGameCode(string code)
+        {
+            if (string.IsNullOrWhiteSpace(code))
+                return false;
+            
+            if (code.Length != 8)
+                return false;
+
+            for (int i = 0; i < 8; i += 2)
+            {
+                int number = GetNumber(code[i], code[i + 1]);
+                if (number < 0 || number > 255)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ UnityProject/Assets/Tests/IpCodeSystemTest.cs
@@ -0,0 +1,63 @@
+using NUnit.Framework;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Victorina.Test
+{
+    public class IpCodeSystemTest
+    {
+        [Test]
+        public void GetCodeTest()
+        {
+            IpCodeSystem s = new IpCodeSystem();
+            
+            Assert.AreEqual(s.GetCode("0.0.0.0"), "AAAAAAAA");
+            Assert.AreEqual(s.GetCode("255.255.255.255"), "VJVJVJVJ");
+            Assert.AreEqual(s.GetCode("31.41.196.195"), "FBPBOHNH");
+            Assert.AreEqual(s.GetCode("10.0.0.109"), "KAAAAAFE");
+            Assert.AreEqual(s.GetCode("94.232.233.226"), "QDYIZISI");
+            Assert.AreEqual(s.GetCode("127.0.0.1"), Static.LocalhostGameCode);
+        }
+
+        [Test]
+        public void GetCodeWrongTest()
+        {
+            IpCodeSystem s = new IpCodeSystem();
+            
+            string code1 = s.GetCode(string.Empty);
+            // string code2 = s.GetCode("1000.1000.1000.1000");
+            // string code3 = s.GetCode("GiveMeCodePlease");
+        }
+
+        [Test]
+        public void GetIpTest()
+        {
+            IpCodeSystem s = new IpCodeSystem();
+
+            Assert.AreEqual(s.GetIp("AAAAAAAA"), "0.0.0.0");
+            Assert.AreEqual(s.GetIp("VJVJVJVJ"), "255.255.255.255");
+            Assert.AreEqual(s.GetIp("FBPBOHNH"), "31.41.196.195");
+            Assert.AreEqual(s.GetIp("KAAAAAFE"), "10.0.0.109");
+            Assert.AreEqual(s.GetIp("QDYIZISI"), "94.232.233.226");
+        }
+
+        [Test]
+        public void IsGameCodeValidTest()
+        {
+            IpCodeSystem s = new IpCodeSystem();
+
+            Assert.IsFalse(s.IsValidGameCode(null));
+            Assert.IsFalse(s.IsValidGameCode(""));
+            Assert.IsFalse(s.IsValidGameCode("12345678"));
+            Assert.IsFalse(s.IsValidGameCode("ZZZZZZZZ"));
+            Assert.IsFalse(s.IsValidGameCode("AaAaAaAa"));
+            Assert.IsFalse(s.IsValidGameCode("ABABABABABAB"));
+            Assert.IsFalse(s.IsValidGameCode("AZAZAZAZ"));
+            
+            Assert.IsTrue(s.IsValidGameCode("AAAAAAAA"));
+            Assert.IsTrue(s.IsValidGameCode("QDYIZISI"));
+            Assert.IsTrue(s.IsValidGameCode("ABABABAB"));
+            Assert.IsTrue(s.IsValidGameCode("ABBBCBDB"));
+            Assert.IsTrue(s.IsValidGameCode("VJVJVJVJ"));
+        }
+    }
+}
