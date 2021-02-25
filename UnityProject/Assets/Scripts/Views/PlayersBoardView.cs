ccc2f27e20ceb7751b3e8316c5e32eaa73545801
Oleg Andrushko <nubick@gmail.com>
2021-02-25 14:17:23 +0700

    Optimization: Players board view widget caching instead every update instantiating.

--- UnityProject/Assets/Scripts/Views/PlayersBoardView.cs
+++ UnityProject/Assets/Scripts/Views/PlayersBoardView.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Injection;
 using UnityEngine;
 
@@ -20,16 +22,34 @@ namespace Victorina
 
         protected override void OnShown()
         {
+            _widgets.Clear();
+            ClearChild(WidgetsRoot);
             RefreshUI();
         }
 
+        private readonly List<PlayerBoardWidget> _widgets = new List<PlayerBoardWidget>();
+        
         private void RefreshUI()
         {
-            ClearChild(WidgetsRoot);
             PlayersBoard playersBoard = MatchData.PlayersBoard.Value;
-            foreach (PlayerData player in playersBoard.Players)
+
+            while (_widgets.Count < playersBoard.Players.Count)
             {
                 PlayerBoardWidget widget = Instantiate(WidgetPrefab, WidgetsRoot);
+                _widgets.Add(widget);
+            }
+
+            while (_widgets.Count > playersBoard.Players.Count)
+            {
+                PlayerBoardWidget widget = _widgets.Last();
+                _widgets.Remove(widget);
+                Destroy(widget.gameObject);
+            }
+
+            for (int i = 0; i < playersBoard.Players.Count; i++)
+            {
+                PlayerBoardWidget widget = _widgets[i];
+                PlayerData player = playersBoard.Players[i];
                 widget.Bind(player, playersBoard.Current == player);
             }
         }
