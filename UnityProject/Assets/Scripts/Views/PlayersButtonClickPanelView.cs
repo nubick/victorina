98a870d2f24affc3941e3b43babb4d94d7372d9e
Oleg Andrushko <nubick@gmail.com>
2021-04-20 21:52:32 +0700

    Master: Most fastest player button.

--- UnityProject/Assets/Scripts/Views/PlayersButtonClickPanelView.cs
+++ UnityProject/Assets/Scripts/Views/PlayersButtonClickPanelView.cs
@@ -1,5 +1,6 @@
 using Injection;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Victorina
 {
@@ -7,9 +8,13 @@ namespace Victorina
     {
         [Inject] private QuestionAnswerSystem QuestionAnswerSystem { get; set; }
         [Inject] private QuestionAnswerData QuestionAnswerData { get; set; }
+        [Inject] private NetworkData NetworkData { get; set; }
         
         public RectTransform WidgetsRoot;
         public PlayerButtonClickWidget WidgetPrefab;
+
+        public RectTransform VerticalLayoutGroup;
+        public GameObject SelectFastestButton;
         
         public void Initialize()
         {
@@ -27,11 +32,19 @@ namespace Victorina
                 PlayerButtonClickWidget widget = Instantiate(WidgetPrefab, WidgetsRoot);
                 widget.Bind(player);
             }
+
+            SelectFastestButton.SetActive(NetworkData.IsMaster && QuestionAnswerData.PlayersButtonClickData.Players.Count > 1);
+            LayoutRebuilder.ForceRebuildLayoutImmediate(VerticalLayoutGroup);
         }
 
         private void OnWidgetClicked(PlayerButtonClickData playerData)
         {
             QuestionAnswerSystem.SelectPlayerForAnswer(playerData.PlayerId);
+        }
+
+        public void OnSelectFastestButtonClicked()
+        {
+            QuestionAnswerSystem.SelectFastestPlayerForAnswer();   
         }
     }
 }
