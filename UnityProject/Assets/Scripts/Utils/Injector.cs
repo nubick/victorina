26d7d827967e4da3b78ab01b15cd72782c928fc1
Oleg Andrushko <nubick@gmail.com>
2021-06-26 17:51:13 +0800

    bug-fix: Injector was updated to support classes inheritance with injecting fields and properties.

--- UnityProject/Assets/Scripts/Utils/Injector.cs
+++ UnityProject/Assets/Scripts/Utils/Injector.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Injection
 {
 	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
 	public class Inject : Attribute { }
-	
+
 	public class Injector
 	{
 		private static readonly Dictionary<Type, FieldInfo[]> FieldsCache = new Dictionary<Type, FieldInfo[]>();
 		private static readonly Dictionary<Type, PropertyInfo[]> PropertiesCache = new Dictionary<Type, PropertyInfo[]>();
 
 		private readonly Dictionary<Type, object> _objects = new Dictionary<Type, object>();
+
+		private BindingFlags BindingFlags => BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
 		
 		public void Bind<T>(T obj)
 		{
 			Type type = typeof(T);
-			
+
 			if (_objects.ContainsKey(type))
 				throw new ArgumentException($"Injector contains instance of type '{type.FullName}'.");
 			_objects[type] = obj;
@@ -44,11 +48,11 @@ namespace Injection
 		public void InjectTo(object obj)
 		{
 			Type objType = obj.GetType();
-			
+
 			foreach (FieldInfo fieldInfo in GetFields(objType))
 			{
 				object value = Get(fieldInfo.FieldType);
-				
+
 				if (value == null)
 					Debug.Log($"Injecting:[WARNING]: Can't find type {fieldInfo.FieldType.FullName} for object: {obj}.");
 
@@ -61,7 +65,7 @@ namespace Injection
 
 				if (value == null)
 					Debug.Log($"Injecting:[WARNING]: Can't find type {propertyInfo.PropertyType.FullName} for object: {obj}.");
-				
+
 				propertyInfo.SetValue(obj, value);
 			}
 		}
@@ -76,10 +80,10 @@ namespace Injection
 		public T Get<T>()
 		{
 			object obj = Get(typeof(T));
-			
+
 			if (obj == null)
 				Debug.Log($"Injecting:[WARNING]: Can't find type {typeof(T)}.");
-			
+
 			return (T) obj;
 		}
 
@@ -91,23 +95,27 @@ namespace Injection
 		private FieldInfo[] GetFields(Type type)
 		{
 			if (!FieldsCache.ContainsKey(type))
-			{
-				FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-					.Where(fieldInfo => fieldInfo.IsDefined(typeof(Inject), inherit: false)).ToArray();
-				FieldsCache[type] = fields;
-			}
+				FieldsCache[type] = GetMemberInfos(type, t => t.GetFields(BindingFlags));
 			return FieldsCache[type];
 		}
 
 		private PropertyInfo[] GetProperties(Type type)
 		{
 			if (!PropertiesCache.ContainsKey(type))
-			{
-				PropertyInfo[] fields = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-					.Where(fieldInfo => fieldInfo.IsDefined(typeof(Inject), inherit: false)).ToArray();
-				PropertiesCache[type] = fields;
-			}
+				PropertiesCache[type] = GetMemberInfos(type, t => t.GetProperties(BindingFlags));
 			return PropertiesCache[type];
+		}
+
+		private T[] GetMemberInfos<T>(Type type, Func<Type, T[]> getMembersFunc) where T : MemberInfo
+		{
+			List<T> membersList = new List<T>();
+			while (type != typeof(object))
+			{
+				var fields = getMembersFunc(type).Where(fieldInfo => fieldInfo.IsDefined(typeof(Inject), inherit: false));
+				membersList.AddRange(fields);
+				type = type.BaseType;
+			}
+			return membersList.ToArray();
 		}
 	}
 }
